935a5887103fe3accb46ad88ccc51c45c21f1481
Sam <samarooniii@yahoo.com>
2023-07-28 16:03:35 -0400

    select favorite photo, remove photo

--- client/src/components/PhotoUploader.jsx
+++ client/src/components/PhotoUploader.jsx
@@ -38,6 +38,17 @@ const PhotoUploader = ({ addPhotos, onChange }) => {
         });
       });
   };
+
+  const removePhoto = (e, filename) => {
+    e.preventDefault();
+    onChange([...addPhotos.filter((photo) => photo !== filename)]);
+  };
+
+  const selectAsMainPhoto = (e, filename) => {
+    e.preventDefault();
+
+    onChange([filename, ...addPhotos.filter((photo) => photo !== filename)]);
+  };
   return (
     <>
       <div className="flex gap-2">
@@ -58,11 +69,61 @@ const PhotoUploader = ({ addPhotos, onChange }) => {
       <div className="mt-3 grid gap-2 grid-cols-3 lg:grid-cols-6 md:grid-cols-4">
         {addPhotos?.length > 0 &&
           addPhotos.map((link, i) => (
-            <div className="h-32 flex" key={i}>
+            <div className="h-32 flex relative" key={i}>
               <img
                 className="rounded-2xl w-fullobject-cover"
                 src={"http://localhost:4000/uploads/" + link}
               />
+              <button
+                onClick={(e) => removePhoto(e, link)}
+                className="cursor-pointer absolute bottom-1 right-2 text-white bg-black bg-opacity-50 p-1 rounded-2xl py-2 px-3 "
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                  />
+                </svg>
+              </button>
+              <button
+                onClick={(e) => selectAsMainPhoto(e, link)}
+                className="cursor-pointer absolute bottom-1 left-2 text-white bg-black bg-opacity-50 p-1 rounded-2xl py-2 px-3 "
+              >
+                {link === addPhotos[0] && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                  </svg>
+                )}
+                {link !== addPhotos[0] && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                    />
+                  </svg>
+                )}
+              </button>
             </div>
           ))}
         <label className="h-32 cursor-pointer flex items-center justify-center gap-1 border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
