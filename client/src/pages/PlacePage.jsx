e6a0faa8cbbabcddf64b93a15160b4a184c38899
Sam <samarooniii@yahoo.com>
2023-07-30 14:04:56 -0400

    working on booking functionality

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -25,7 +25,7 @@ const PlacePage = () => {
       <div className="absolute inset-0 bg-black text-white min-h-screen">
         <div className="p-8 grid gap-4 bg-black">
           <div className="font-lato font-regular">
-            <h2 className="text-3xl">Photos of {place.title}</h2>
+            <h2 className="text-3xl mr-48">Photos of {place.title}</h2>
             <button
               onClick={() => setShowAllPhotos(false)}
               className="fixed right-12 top-8 flex gap-1 py-2 px-4 rounded-2xl shadow shadow-black bg-white text-black"
@@ -59,7 +59,7 @@ const PlacePage = () => {
   }
 
   return (
-    <div className="font-lato font-regular mt-4  mx-32 py-4">
+    <div className="font-lato font-regular mt-4  mx-32 pt-4">
       <h1 className="text-3xl">{place.title}</h1>
       <a
         href={`https://maps.google.com/?q=${place.address}`}
@@ -93,7 +93,8 @@ const PlacePage = () => {
             {place.photos?.[0] && (
               <div>
                 <img
-                  className="aspect-square object-cover"
+                  onClick={() => setShowAllPhotos(true)}
+                  className="aspect-square object-cover cursor-pointer"
                   src={`http://localhost:4000/uploads/${place.photos[0]}`}
                 />
               </div>
@@ -102,14 +103,16 @@ const PlacePage = () => {
           <div className="grid">
             {place.photos?.[1] && (
               <img
-                className="aspect-square object-cover"
+                onClick={() => setShowAllPhotos(true)}
+                className="aspect-square object-cover cursor-pointer"
                 src={`http://localhost:4000/uploads/${place.photos[1]}`}
               />
             )}
             <div className="overflow-hidden">
               {place.photos?.[2] && (
                 <img
-                  className="aspect-square object-cover relative top-2"
+                  onClick={() => setShowAllPhotos(true)}
+                  className="aspect-square object-cover relative top-2 cursor-pointer"
                   src={`http://localhost:4000/uploads/${place.photos[2]}`}
                 />
               )}
@@ -147,12 +150,17 @@ const PlacePage = () => {
           Check-out time: {place.checkOut}
           <br />
           Max number of guests: {place.maxGuests}
-          <div className="text-sm text-gray-700 leading-4 mt-2">
-            {place.extraInfo}
-          </div>
         </div>
         <div>
           <BookingWidget place={place} />
+        </div>
+      </div>
+      <div className="bg-white -mx-8 px-8 py-8">
+        <div>
+          <h2 className="font-bold text-xl">Extra Info</h2>
+        </div>
+        <div className="text-sm text-gray-700 leading-4 mb-4 mt-1">
+          {place.extraInfo}
         </div>
       </div>
     </div>
